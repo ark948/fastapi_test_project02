adbecc6cb887b031651a91f7f0b513c3c82d7a33
ark948 <armankarimi948@gmail.com>
2024-10-23 19:42:05 +0330

    Base fastapi project

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read():
+    return {"hello": "world"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
